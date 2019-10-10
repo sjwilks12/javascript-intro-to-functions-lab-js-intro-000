d5d1b09d415307a5bce57876b4952c8bf896f308
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2019-10-10 21:52:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
   //var lowercase = "hello"
   if string === string.toUpperCase {
     return "I can't hear you!"
-  } 
+  }
   else if string === string.toLowerCase {
     return "YES INDEED!"
   }
